7e8b6ee3180957c02a5b5d1d6fee7bf1b9e37221
Experiencepwunkr <99370318+Experiencepwunkr@users.noreply.github.com>
2025-09-28 15:51:20 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/itech-website/',   // 👈 IMPORTANT: repo name
   plugins: [react()],
 })
